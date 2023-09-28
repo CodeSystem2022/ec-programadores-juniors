475bc54cf19169bc06da6759bc81d1eab26c3d41
EmirMaya <emirmayaok@gmail.com>
2023-09-28 02:26:55 -0300

    logo actualizado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const NavbarComponent = () => {
                         <Offcanvas.Header className='off-header' closeButton>
                             <Offcanvas.Title className='off-title' id={`offcanvasNavbarLabel-expand-lg`}>
                                 <img className='img-logo'
-                                    src="./logo/sportblend.png"
+                                    src="./logo/sportblend-logo.png"
                                     alt="logo"
                                 />
                             </Offcanvas.Title>
@@ -120,7 +120,7 @@ const NavbarComponent = () => {
 
             <Navbar.Brand className='navbar-brand' href="./">
                 <img className='img-logo'
-                    src="./logo/sportblend.png"
+                    src="./logo/sportblend-logo.png"
                     alt="logo"
                 />
             </Navbar.Brand>
