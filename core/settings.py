a2e97eb8197aab5b11b5fa063f8db08ab3592f06
gastonfr24 <gastonfr24@gmail.com>
2023-09-29 11:59:52 -0300

    new-feature: user | models y serializers

--- core/settings.py
+++ core/settings.py
@@ -213,10 +213,11 @@ DJOSER = {
     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
     'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:8000/google', 'http://localhost:8000/facebook'],
     'SERIALIZERS': {
-        # 'user_create': 'apps.user.serializers.UserCreateSerializer',
-        # 'user': 'apps.user.serializers.UserCreateSerializer',
-        # 'current_user': 'apps.user.serializers.UserCreateSerializer',
-        # 'user_delete': 'djoser.serializers.UserDeleteSerializer',
+        "user_create": "apps.user.serializers.UserSerializer",
+        "user": "apps.user.serializers.UserSerializer",
+        "current_user": "apps.user.serializers.UserSerializer",
+        "user_delete": "djoser.serializers.UserDeleteSerializer",
+        "password_reset_confirm": "apps.user.serializers.CustomPasswordResetConfirmSerializer",
     },
 }
 
