e667cc4eaca9633b5438f8c3f137630ef30dcc66
gastonfr24 <gastonfr24@gmail.com>
2023-09-29 12:52:00 -0300

    fix: redux | enrutado de action y reducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 import "./App.scss";
-import { Route, Routes } from "react-router-dom";
+// Rutas
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store.js";
+
+// Containers Page
 import Home from "./containers/Home/Home";
+import Error404 from './containers/errors/Error404'
 
 function App() {
   return (
-    <>
-      <Routes>
+    <Provider store={store}>
+      <Router>
+       <Routes>
+
+          {/* Error 404 */}
+       <Route path="*" element={<Error404 />} />
+
+          {/* Home */}
         <Route path="/" element={<Home />} />
-      </Routes>
-    </>
+
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import reducer from "./reducer";
-import thunk from "redux-thunk";
-
-const composeEnhancer = window.REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
-
-var store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)));
-
-export default store;
