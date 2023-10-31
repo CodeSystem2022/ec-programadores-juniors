33c676bbd4774b5396568b8b366009d339d70bf9
biancatosetto <biancatosetto@gmail.com>
2023-10-30 23:20:50 -0300

    About/Error404

--- src/containers/About/About.jsx
+++ src/containers/About/About.jsx
@@ -10,13 +10,13 @@ function About() {
                 </h1>
 
                 <div className='mx-2'>
-                    <p className='txt'>En Sport Blend, fusionamos nuestra pasión por el deporte y la moda para ofrecerte una experiencia única en la compra de ropa deportiva moderna. </p>
+                    <p className='txt'>En SportBlend fusionamos nuestra pasión por el deporte y la moda para ofrecerte una experiencia única en la compra de ropa deportiva moderna. </p>
                     <div className='d-flex justify-content-center about-cont'>
                         <div className='img-div'>
                             <img className='about-img' src="./logo/logo.png" alt="runman" />
                             <div className='d-flex justify-content-around sqs-div'>
                                 <div className='first-sq'>
-                                    <p className='text-p'>Sportblend: donde la moda deportiva y casual se encuentran en armonía.</p>
+                                    <p className='text-p'>SportBlend, donde la moda deportiva y casual se encuentran en armonía.</p>
                                 </div>
                                 <div className='second-sq'>
                                     <p className='text-p'>Exploramos la simplicidad en el estilo de vida activo, ofreciendo productos de calidad. </p>
