691732bd10569c31e346ec02466393e67220fed3
EmirMaya <emirmayaok@gmail.com>
2023-11-02 20:51:18 -0300

    llamados de products desde el back local

--- src/components/ProductsCarousel/ProductsCarousel.jsx
+++ src/components/ProductsCarousel/ProductsCarousel.jsx
@@ -1,29 +1,18 @@
-
+import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import SimpleProduct from '../SimpleProductComp/SimpleProduct';
 import './ProductsCarousel.scss';
+import Axios from 'axios';
 
 const ProductsCarousel = () => {
-    const products = [
-        {
-            id: 1,
-            name: "Zapatilla Saucony Axon 3",
-            price: 70000,
-            img: './img/prod-test.jpg'
-        },
-        {
-            id: 2,
-            name: "Zapatilla Saucony Axon 3",
-            price: 80000,
-            img: './img/prod-test2.jpg'
-        },
-        {
-            id: 3,
-            name: "Zapatilla Saucony Axon 3",
-            price: 90000,
-            img: './img/prod-test.jpg'
-        }
-    ]
+
+    const [products, setProducts] = useState([])
+
+    Axios.get(`${import.meta.env.VITE_APP_HOST}/product/get-products`).then(response => {
+        // console.log(response.data.products)
+        setProducts(response.data.products);
+    })
+
     return (
         <Carousel>
             {products.map((product, index) => (
