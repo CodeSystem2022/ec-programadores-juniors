91a707dcdd0820a696b2da3daf019900daab9947
EmirMaya <emirmayaok@gmail.com>
2023-11-08 12:09:30 -0300

    cambio de rutas y haciendo filtros en products

--- src/containers/Products/Products.jsx
+++ src/containers/Products/Products.jsx
@@ -6,6 +6,22 @@ import Options from "../../components/Options/Options";
 
 const Products = () => {
   const [products, setProducts] = useState([]);
+  const [calzadoMujer, setCalzadoMujer] = useState(null);
+   const [calzadoHombre, setCalzadoHombre] = useState(null);
+   const [calzadoChild, setCalzadoChild] = useState(null);
+
+
+  Axios.get(
+    `${import.meta.env.VITE_APP_HOST}/category/categories`
+  ).then(res => {
+    console.log(res.data.categories[0].sub_categories[0].name)
+    setCalzadoHombre(res.data.categories[0].sub_categories[0].name);
+    setCalzadoMujer(res.data.categories[0].sub_categories[1].name);
+    setCalzadoChild(res.data.categories[0].sub_categories[2].name);
+  });
+
+
+
 
   useEffect(() => {
     async function fetchData() {
@@ -14,6 +30,7 @@ const Products = () => {
           `${import.meta.env.VITE_APP_HOST}/product/get-products`
         );
         setProducts(response.data.products);
+        console.log(response.data.products)
       } catch (error) {
         console.log(error);
       }
@@ -30,10 +47,10 @@ const Products = () => {
           {products.map((product, index) => (
             <CardProduct
               key={index}
-              id={product.id}
-              name={product.name}
-              price={product.price}
-              img={product.img}
+              id={product?.id}
+              name={product?.name}
+              price={product?.price}
+              img={product?.photo}
             />
           ))}
         </div>
